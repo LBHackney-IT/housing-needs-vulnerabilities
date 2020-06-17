64f59e245f789fa6591164958e80204c8ae8b060
Matt Millar <matthew.millar@madetech.com>
2020-06-17 23:05:50 +0100

    Added accordion component

---
+++ components/Form/Accordion/AccordionItem/index.js
@@ -0,0 +1,16 @@
+const AccordionItem = ({ children, heading, id }) => (
+  <div className="govuk-accordion__section ">
+    <div className="govuk-accordion__section-header">
+      <h3 className="govuk-accordion__section-heading">
+        <span className="govuk-accordion__section-button" id={id}>
+          {heading}
+        </span>
+      </h3>
+    </div>
+    <div className="govuk-accordion__section-content" aria-labelledby={id}>
+      {children}
+    </div>
+  </div>
+);
+
+export default AccordionItem;
