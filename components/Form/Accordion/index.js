4213d7a8c0dc9fc7caf168c9d98ae5b0ec8573bf
Matt Millar <matthew.millar@madetech.com>
2020-06-18 18:56:33 +0100

    Update checkbox and accordion components

--- components/Form/Accordion/index.js
+++ components/Form/Accordion/index.js
@@ -1,5 +1,7 @@
 const Accordion = ({ children }) => (
-  <div className="govuk-accordion">{children}</div>
+  <div className="govuk-accordion" data-module="govuk-accordion">
+    {children}
+  </div>
 );
 
 export default Accordion;
--- components/Form/Checkbox/index.js
+++ components/Form/Checkbox/index.js
@@ -1,25 +1,18 @@
-import { useState } from 'react';
-
 const Checkbox = ({ checked, label, name, onClick, ...others }) => {
-  const [checkedValue, setCheckedValue] = useState(checked ? checked : false);
   return (
-    <div className="govuk-form-group">
-      <div className="govuk-checkboxes">
-        <div className="govuk-checkboxes__item">
-          <input
-            className="govuk-checkboxes__input"
-            id={name}
-            name={name}
-            type="checkbox"
-            onClick={onClick}
-            defaultChecked={checkedValue}
-            {...others}
-          />
-          <label className="govuk-label govuk-checkboxes__label" htmlFor={name}>
-            {label}
-          </label>
-        </div>
-      </div>
+    <div className="govuk-checkboxes__item">
+      <input
+        className="govuk-checkboxes__input"
+        id={name}
+        name={name}
+        type="checkbox"
+        onClick={onClick}
+        defaultChecked={checked}
+        {...others}
+      />
+      <label className="govuk-label govuk-checkboxes__label" htmlFor={name}>
+        {label}
+      </label>
     </div>
   );
 };
