40590c9009a691cde176877c0535ba9639b9b6bd
Matt Millar <matthew.millar@madetech.com>
2020-06-19 12:26:34 +0100

    Update TextArea component to return value onChange

--- components/Form/TextArea/index.js
+++ components/Form/TextArea/index.js
@@ -1,18 +1,23 @@
-const TextArea = ({ label, name, onChange, value }) => (
-  <div className="govuk-form-group">
-    <label className="govuk-label" htmlFor={`${name}`}>
-      {label}
-    </label>
-    <textarea
-      className="govuk-textarea"
-      id={name}
-      name={name}
-      data-testid={name}
-      rows="5"
-      onChange={onChange}
-      value={value}
-    ></textarea>
-  </div>
-);
+const TextArea = ({ label, name, onChange, value }) => {
+  const updateValue = e => {
+    onChange(e.currentTarget.value);
+  };
+  return (
+    <div className="govuk-form-group">
+      <label className="govuk-label" htmlFor={`${name}`}>
+        {label}
+      </label>
+      <textarea
+        className="govuk-textarea"
+        id={name}
+        name={name}
+        data-testid={name}
+        rows="5"
+        onChange={updateValue}
+        value={value}
+      ></textarea>
+    </div>
+  );
+};
 
 export default TextArea;
--- components/Form/TextArea/index.test.js
+++ components/Form/TextArea/index.test.js
@@ -1,19 +1,31 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import TextArea from './index';
 
 describe('TextArea', () => {
   it('renders a text area', () => {
     const inputName = 'input-name';
-    const inputLabel = 'Label';
+    const inputLabel = 'label';
+
     const { getByLabelText } = render(
       <TextArea name={inputName} label={inputLabel} />
     );
 
-    const labelRegex = new RegExp(`\s*${inputLabel}\s*`);
-    const input = getByLabelText(labelRegex);
-
+    const input = getByLabelText(inputLabel);
     expect(input).toBeInTheDocument();
     expect(input.id).toEqual(inputName);
     expect(input.name).toEqual(inputName);
+  });
+
+  it('calls onChange with the new value', () => {
+    const onChange = jest.fn();
+
+    const { getByLabelText } = render(
+      <TextArea name="x" label="x" onChange={onChange} />
+    );
+    fireEvent.change(getByLabelText('x'), {
+      target: { value: 'hello' }
+    });
+
+    expect(onChange).toHaveBeenCalledWith('hello');
   });
 });
