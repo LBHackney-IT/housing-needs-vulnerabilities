597bef44cde6cb0beef5b31ad07e438f55e61f5c
Matt Millar <matthew.millar@madetech.com>
2020-06-19 10:06:59 +0100

    Update grid to import grid data from json file

--- components/Form/CheckboxList/index.test.js
+++ components/Form/CheckboxList/index.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import CheckboxList from './index';
 
-describe('Checkbox', () => {
+describe('CheckboxList', () => {
   it('renders a checkbox list', () => {
     const { container } = render(<CheckboxList />);
     expect(container.querySelector('.govuk-checkboxes')).toBeInTheDocument();
