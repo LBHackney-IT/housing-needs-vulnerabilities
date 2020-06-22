44397831b3882ada9b4870a03a353daaa5f87732
Matt Millar <matthew.millar@madetech.com>
2020-06-22 09:02:27 +0100

    Add update snapshot api and associated use-case and gateway function

--- pages/api/snapshots/[id]/index.test.js
+++ pages/api/snapshots/[id]/index.test.js
@@ -1,48 +1,84 @@
 import { endpoint } from './index';
 import createMockResponse from 'lib/api/utils/createMockResponse';
 
-describe('Get Snapshot Api', () => {
+describe('Get/Update Snapshot Api', () => {
   const id = '1';
   const getSnapshot = { execute: jest.fn(() => ({ id })) };
-  const call = async ({ method, params }) => {
+  const updateSnapshot = { execute: jest.fn(() => ({ id })) };
+  const call = async ({ body, method, params }) => {
     const response = createMockResponse();
-    await endpoint({ getSnapshot })(
+    await endpoint({ getSnapshot, updateSnapshot })(
       {
-        method: method || 'GET',
-        query: params
+        body,
+        query: params,
+        method
       },
       response
     );
     return response;
   };
 
-  it('can get a snapshot', async () => {
-    const response = await call({ params: { id } });
-    expect(getSnapshot.execute).toHaveBeenCalledWith({ id });
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(JSON.stringify({ id }));
+  describe('Get', () => {
+    it('can get a snapshot', async () => {
+      const response = await call({ method: 'GET', params: { id } });
+      expect(getSnapshot.execute).toHaveBeenCalledWith({ id });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(JSON.stringify({ id }));
+    });
+
+    it('accepts GET requests', async () => {
+      const response = await call({ method: 'GET' });
+      expect(response.statusCode).not.toBe(405);
+    });
+
+    it('does not accept other requests', async () => {
+      const response = await call({ method: 'POST' });
+      expect(response.statusCode).toBe(405);
+    });
+
+    it('returns a 500 for other errors', async () => {
+      getSnapshot.execute = jest.fn(() => {
+        throw new Error();
+      });
+      const response = await call({
+        method: 'GET',
+        params: { id }
+      });
+      expect(response.statusCode).toBe(500);
+    });
   });
 
-  it('does not accept non-GET requests', async () => {
-    const response = await call({ method: 'POST' });
-    expect(response.statusCode).toBe(405);
-  });
+  describe('Update', () => {
+    it('can update a snapshot', async () => {
+      const snapshot = { id: 1 };
+      const response = await call({
+        method: 'PATCH',
+        body: snapshot,
+        params: { id }
+      });
+      expect(updateSnapshot.execute).toHaveBeenCalledWith({ snapshot });
+      expect(response.statusCode).toBe(204);
+    });
 
-  it('returns 400 when no id', async () => {
-    const response = await call({
-      params: {}
+    it('accepts PATCH requests', async () => {
+      const response = await call({ method: 'PATCH' });
+      expect(response.statusCode).not.toBe(405);
     });
-    expect(response.statusCode).toBe(400);
-    expect(JSON.parse(response.body).errors.length).toBe(1);
-  });
 
-  it('returns a 500 for other errors', async () => {
-    getSnapshot.execute = jest.fn(() => {
-      throw new Error();
+    it('does not accept other requests', async () => {
+      const response = await call({ method: 'POST' });
+      expect(response.statusCode).toBe(405);
     });
-    const response = await call({
-      params: { id }
+
+    it('returns a 500 for other errors', async () => {
+      updateSnapshot.execute = jest.fn(() => {
+        throw new Error();
+      });
+      const response = await call({
+        method: 'PATCH',
+        params: { id }
+      });
+      expect(response.statusCode).toBe(500);
     });
-    expect(response.statusCode).toBe(500);
   });
 });
