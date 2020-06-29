485e4d894ba4b5f5648e93d0711ec8d1c9559268
Tom Davies <tom.davies@madetech.com>
2020-06-29 10:27:27 +0100

    Adds endpoint for retrieving resources

--- setupTests.js
+++ setupTests.js
@@ -7,3 +7,12 @@ global.console = {
   info: console.info,
   debug: console.debug
 };
+
+// These are required in order to initialise the Airtable API
+// inside of `ResourcesGateway`. They need to be present before
+// it is required inside of dependencies.js.
+process.env = Object.assign(process.env, {
+  AIRTABLE_API_KEY: 'not.a.real.api.key',
+  AIRTABLE_BASE_ID: 'not.a.real.base.id',
+  AIRTABLE_TABLE_NAMES: 'not.a.real.table.name'
+});
