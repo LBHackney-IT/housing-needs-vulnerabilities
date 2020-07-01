9c2de85b6efb2b978ffa99b0eccd5b649b900c88
Tom Davies <tom.davies@madetech.com>
2020-07-01 14:40:52 +0100

    Retrieves and passes resources data to grid

--- pages/snapshots/[id].js
+++ pages/snapshots/[id].js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import useSnapshot from 'lib/api/utils/useSnapshot';
-import { requestSnapshot } from 'lib/api';
+import { requestSnapshot, requestResources } from 'lib/api';
 import HttpStatusError from 'lib/api/domain/HttpStatusError';
 import { getTokenFromCookieHeader } from 'lib/utils/token';
 import { Button, TextArea } from 'components/Form';
 import VulnerabilitiesGrid from 'components/Feature/VulnerabilitiesGrid';
 import { convertIsoDateToString, convertIsoDateToYears } from 'lib/utils/date';
 
-const SnapshotSummary = ({ initialSnapshot, token }) => {
+const SnapshotSummary = ({ resources, initialSnapshot, token }) => {
   const { snapshot, loading, updateSnapshot } = useSnapshot(
     initialSnapshot.snapshotId,
     {
@@ -22,8 +22,8 @@ const SnapshotSummary = ({ initialSnapshot, token }) => {
 
   const [editSnapshot, setEditSnapshot] = useState(
     snapshot.assets.length === 0 &&
-      snapshot.vulnerabilities.length === 0 &&
-      !snapshot.notes
+    snapshot.vulnerabilities.length === 0 &&
+    !snapshot.notes
   );
 
   const updateSelected = selected => {
@@ -62,7 +62,10 @@ const SnapshotSummary = ({ initialSnapshot, token }) => {
       )}
       {editSnapshot && (
         <>
-          <VulnerabilitiesGrid onUpdate={updateSelected} />
+          <VulnerabilitiesGrid
+            onUpdate={updateSelected}
+            resources={resources}
+          />
           <TextArea
             name="notes"
             label="Any other notes you'd like to add?"
@@ -89,8 +92,8 @@ const SnapshotSummary = ({ initialSnapshot, token }) => {
                 ))}
               </ul>
             ) : (
-              'None captured'
-            )}
+                'None captured'
+              )}
           </div>
           <div data-testid="assets-summary">
             <h2>Assets</h2>
@@ -101,8 +104,8 @@ const SnapshotSummary = ({ initialSnapshot, token }) => {
                 ))}
               </ul>
             ) : (
-              'None captured'
-            )}
+                'None captured'
+              )}
           </div>
           <div data-testid="notes-summary">
             <h2>Notes</h2>
@@ -122,7 +125,9 @@ SnapshotSummary.getInitialProps = async ({
   try {
     const token = getTokenFromCookieHeader(headers);
     const initialSnapshot = await requestSnapshot(id, { token });
+    const resources = await requestResources({ token });
     return {
+      resources,
       initialSnapshot,
       token
     };
