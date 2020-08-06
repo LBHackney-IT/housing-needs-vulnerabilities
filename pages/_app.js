800ae44f9bfd480e2ae936c29974a60941a041f6
Matt Millar <matthew.millar@madetech.com>
2020-08-06 13:19:57 +0100

    Remove cookie banner and update service name

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import App from 'next/app';
+import TagManager from 'react-gtm-module';
 import Layout from 'components/Layout';
 import './stylesheets/all.scss';
 
 export default class MyApp extends App {
   componentDidMount = () => {
     window.GOVUKFrontend.initAll();
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GTM_ID
+    });
   };
 
   render() {
